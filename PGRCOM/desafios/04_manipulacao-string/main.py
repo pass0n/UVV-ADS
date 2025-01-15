f7e1a510ad9458735fcc28898fc3c7bb14b95b41
pass0n <kayllonsv.passon@gmail.com>
2025-01-14 21:08:46 -0300

    Desafio 04: Introdução a python

---
+++ PGRCOM/desafios/04_manipulacao-string/main.py
@@ -0,0 +1,10 @@
+str1: str = "Este é um código "
+str2: str = "escrito na linguagem "
+str3: str = "de programação Python!"
+
+res = str1 + str2 + str3
+
+print(res)
+print(f"20 primeiros caracteres: {res [0:20]}")
+print(len(res))
+print(f"Todos os caracteres: {res [0:60]}")
